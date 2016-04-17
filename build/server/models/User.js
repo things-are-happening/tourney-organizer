f9391dceeaefda6713a8aa000b0cdfe9bb559649
Baruch Velez <baruchvelez@gmail.com>
2016-04-17 15:30:23 -0500

    deleted build

--- build/server/models/User.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzZXJ2ZXIvbW9kZWxzL1VzZXIuanMiLCJzb3VyY2VzQ29udGVudCI6W10sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
