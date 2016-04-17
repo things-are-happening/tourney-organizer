3124380f9f1e32dab9c3da5396095ed4ef78c2a4
Baruch Velez <baruchvelez@gmail.com>
2016-04-17 15:14:05 -0500

    init

---
+++ build/server/models/Tournament.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzZXJ2ZXIvbW9kZWxzL1RvdXJuYW1lbnQuanMiLCJzb3VyY2VzQ29udGVudCI6W10sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
