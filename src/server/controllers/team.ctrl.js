e00fcf6413a1d3fbc95429d1c6619cc62169bbde
Joel Chacon <taktic11@gmail.com>
2016-04-19 09:53:00 -0500

    team controller

---
+++ src/server/controllers/team.ctrl.js
@@ -0,0 +1,27 @@
+import Team from './../models/Team'
+import {crudCreate, crudReadOne} from './../helpers/crud'
+import {generateKey} from './../helpers/utils'
+
+export function createTeam(req, res) {
+	req.body.teamId = generateKey(Team, `teamId`)
+	crudCreate(Team, req.body)
+	.then(team => {
+		return res.status(200).send(team)
+	})
+	.catch(err => {
+		return res.status(400).send(err)
+	})	
+}
+
+export function getTeam(req, res) {
+	let query = {
+		teamId: req.params.teamId
+	}
+	crudReadOne(Team, query)
+	.then(bracket => {
+		return res.status(200).send(team)
+	})
+	.catch(err => {
+		return res.status(400).send(err)
+	})
+}
