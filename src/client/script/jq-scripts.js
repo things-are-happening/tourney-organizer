87109ae1611ccb0800dd0ec886df7cdbf7c82d05
Joseph Nolan <josephn0624@gmail.com>
2016-04-20 17:36:00 -0500

    added tab func to jquery

--- src/client/script/jq-scripts.js
+++ src/client/script/jq-scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
-  
+  $('.bracketTab').each(function(){
+    $(this).on('click', function(ev){
+      ev.preventDefault()
+      var target = '#' + $(this).attr('id')
+      $(target).tab('show')
+    })
+  })
 })
