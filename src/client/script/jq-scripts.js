85f2a7737016e942784b5e5b19992e9f5f0f53a6
Joseph Nolan <josephn0624@gmail.com>
2016-04-21 13:18:40 -0500

    .

--- src/client/script/jq-scripts.js
+++ src/client/script/jq-scripts.js
@@ -1,9 +1,9 @@
-$(document).ready(() => {
-  $('.bracketTab').each(function(){
-    $(this).on('click', function(ev){
-      ev.preventDefault()
-      var target = '#' + $(this).attr('id')
-      $(target).tab('show')
-    })
-  })
-})
+// $(document).ready(() => {
+//   $('.bracketTab').each(function(){
+//     $(this).on('click', function(ev){
+//       ev.preventDefault()
+//       var target = '#' + $(this).attr('id')
+//       $(target).tab('show')
+//     })
+//   })
+// })
